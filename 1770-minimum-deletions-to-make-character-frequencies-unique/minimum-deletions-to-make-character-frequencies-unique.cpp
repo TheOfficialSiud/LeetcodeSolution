805fb9f22afade13302663db09def932db771234
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-09-12 20:19:47 +0530

    Time: 113 ms (20.82%) | Memory: 17.5 MB (22.39%) - LeetSync

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        map<char,int> mp;
+        vector<int> vec;
+        for(int i=0;i<s.length();i++)
+        {
+            mp[s[i]]++;
+        }
+         for(auto it:mp)
+        {
+            vec.push_back(it.second);
+        }
+        sort(vec.begin(),vec.end(),greater<int>());
+        int c=0;
+
+        for(int i=1;i<vec.size();i++){
+            
+            if(vec[i]==vec[i-1])
+            {
+                c++;
+                vec[i]-=1;
+                
+
+                i=0;
+                sort(vec.begin(),vec.end(),greater<int>());
+                if(vec[vec.size()-1]==0)
+                vec.pop_back();
+            }
+        }
+        for(int i=0;i<vec.size();i++)
+        cout<<vec[i]<<" ";
+        return c;
+    }
+};
