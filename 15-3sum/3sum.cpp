40f4d62eb11fafaf49020f07fd35c8652d0a81f6
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-09-14 18:56:38 +0530

    Time: 1776 ms (6.50%) | Memory: 480.7 MB (5.04%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    inline vector<vector<int>> two_sum(int *i,int *j, int sum  ){
+     vector<vector<int>> two_sum(int *i,int *j, int sum  ){
         cout<<*i<<*j<<" ";
         vector<vector<int>> answer;
         while(i<j){
