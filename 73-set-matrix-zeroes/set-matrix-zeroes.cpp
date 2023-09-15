15ac8dad38e7678a322f59356033ca8f9fb4f266
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-09-15 16:55:01 +0530

    Time: 24 ms (7.65%) | Memory: 16.8 MB (6.76%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -8,7 +8,6 @@ public:
                 if(matrix[i][j]==0){
                     cout<<i<<j;
                     for(int k=0;k<m;k++){
-                        //  cout<<i<<j;
                         pair<int,int> pr;
                         pr.first=k;
                         pr.second=j;
@@ -21,20 +20,11 @@ public:
                         pr.second=k;
                         zeros.insert(pr);
                         }
-                    // zeros.insert(pr);
-                    //  pr.first=i;
-                    //     pr.second=k;
-                    // zeros.insert(pr);
-                    // }
-                    // for(int k=0;k<n;k++)
-                    // set.insert({i,k});
             }
             }
         }
 
         for(auto it :zeros){
-            // cout<<it;
-            cout<<it.first<<" "<<it.second<<endl;
             matrix[it.first][it.second]=0;
         }
     }
