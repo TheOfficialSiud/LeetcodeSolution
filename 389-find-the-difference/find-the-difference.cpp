4c25485280dd3a9a3e0f21f5f258c8da7b7a3cda
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-09-25 08:58:23 +0530

    Time: 4 ms (48.11%) | Memory: 6.9 MB (16.46%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-         sort(t.begin(),t.end());
+        // sort(s.begin(),s.end());
+        //  sort(t.begin(),t.end());
+        int sum1=0,sum2=0;
          for(int i=0;i<s.length();i++){
-             if(s[i]!=t[i])
-             return t[i];
+            //  if(s[i]!=t[i])
+            //  return t[i];
+               sum1+=(int)s[i];
+               sum2+=(int)t[i];
+
          }
-         return t[t.length()-1];
+         sum2+=(int)t[s.length()];
+         return char(sum2-sum1);
     }
 };
