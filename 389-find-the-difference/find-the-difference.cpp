06142fd3e2de375832e8f7206c90c300bb82053f
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-09-25 08:50:10 +0530

    Time: 3 ms (65.73%) | Memory: 7 MB (8.68%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -3,7 +3,6 @@ public:
     char findTheDifference(string s, string t) {
         sort(s.begin(),s.end());
          sort(t.begin(),t.end());
-        cout<<s<<endl<<t;
          for(int i=0;i<s.length();i++){
              if(s[i]!=t[i])
              return t[i];
