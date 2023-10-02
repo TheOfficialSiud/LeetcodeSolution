a501fb93f070dc610f4dc5e5e4422068057d872f
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-10-02 10:59:11 +0530

    Time: 1462 ms (5.24%) | Memory: 13.5 MB (33.01%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
 
     bool winnerOfGame(string s) {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
      int a=0,b=0;
     size_t found1=0,found2=0;
     while(1){
