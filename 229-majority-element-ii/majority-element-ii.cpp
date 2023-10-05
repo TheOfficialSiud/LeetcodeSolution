d8d1a59d0662ed6a7267c317b7d87b3ad7a91e8c
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-10-05 08:54:24 +0530

    Time: 9 ms (61.98%) | Memory: 16.2 MB (28.20%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         vector<int> ans;
         int n=nums.size();
-        int occ=n/3,c=1,temp;
-        sort(nums.begin(),nums.end());
-        temp=nums[0];
+        int occ=n/3,c=1,temp=nums[0];;
+       
+        
         for(int i=1;i<n;i++){
             if(nums[i]!=temp){
                 if(c>occ){
@@ -13,14 +14,12 @@ public:
                 }
                 temp=nums[i];
                 c=1;
-                cout<<1;
             }
-            else if(nums[i]==temp){
+            else{
                 c++;
-                cout<<'c';
             }
-            cout<<temp<<" "<<c<<endl;
         }
+        
         if(c>occ){
             ans.push_back(nums.back());
         }
