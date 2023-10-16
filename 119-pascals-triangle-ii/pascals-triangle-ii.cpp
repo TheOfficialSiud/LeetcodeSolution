c742588fa2f57c08d309abde2debfe3159001a96
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-10-16 17:23:20 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (11.01%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> ans;
+        if(rowIndex==0)
+        return {1};
+       ans.push_back(1);
+       ans.push_back(1);
+        if(rowIndex==1)
+        return ans;
+        else{
+            rowIndex++;
+            for(int i=3;i<=rowIndex;i++){
+                vector<int> temp;
+                temp.push_back(1);
+                for(int j=1;j<i-1;j++){
+                        temp.push_back(ans[j-1]+ans[j]);
+                }
+                temp.push_back(1);
+                ans=temp;
+            }
+        }
+        return ans;
+    }
+};
