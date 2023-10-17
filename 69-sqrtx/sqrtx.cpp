2840b1ec8ee5c48117910e24cadc4f3d57ae94d7
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-10-17 18:09:03 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (17.57%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long i=0,j=x,mid=(i+j)/2;
+        int i=0,j=x,mid=(i+j)/2;
         while(i<=j){
-            if(mid*mid==x)
+            long temp=pow(mid,2);
+            if(temp==x)
             return mid;
-            else if (mid*mid>x)
+            else if (temp>x)
             j=mid-1;
             else
             i=mid+1;
             mid=(i+j)/2;
         }
-        cout<<i<<" "<<j<<" "<<mid;
         return mid;
     }
 };
