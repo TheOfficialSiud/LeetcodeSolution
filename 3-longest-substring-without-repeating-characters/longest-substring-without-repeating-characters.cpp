dee45285af1c19edb5f8d54a7e26097c7f516050
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-10-18 21:22:35 +0530

    Time: 797 ms (5.01%) | Memory: 276.6 MB (5.05%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-bool nonrepeatchar(char s1, string s){
-    map<char,int> mp;
-    for(int i=0;i<s.length();i++){
-         if(s[i]==s1)
-         return false;
-    }
-    return true;
- 
-}
 
     int lengthOfLongestSubstring(string s) {
         int i=0;
         string ans="";
+        
         for(int i=0;i<s.length();i++){
             string temp="";
+            set<char> t;
             for(int j=i;j<s.length();j++){
-                if(nonrepeatchar(s[j],temp)){
-                    temp+=s[j];
+                t.insert(s[j]);
+                temp+=s[j];
+                if(t.size()!=temp.length()){
+                    temp.pop_back();
+                     break;
                 }
-                else
-                break;
+               
             }
             if(ans.length()<temp.length())
             ans=temp;
