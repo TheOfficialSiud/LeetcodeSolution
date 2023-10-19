362c5874a33064ebb5060a3e108e608d36f6c2b1
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-10-19 07:29:20 +0530

    Time: 7 ms (8.58%) | Memory: 6.6 MB (54.78%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+string orginal(string s){
+    string ans="";
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='#'){
+            if(!ans.empty()){
+                ans.pop_back();
+            }
+        }
+        else{
+            ans.push_back(s[i]);
+        }
+        cout<<ans<<endl;
+    }
+    return ans;
+}
+    bool backspaceCompare(string s, string t) {
+      s=orginal(s);
+      t=orginal(t);
+      cout<<s<<" "<<t;
+      return (s==t);
+    }
+};
