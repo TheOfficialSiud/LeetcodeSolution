cc37992da891d701af583b0cf66c25a788263c64
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-10-30 17:54:51 +0530

    Time: 45 ms (5.02%) | Memory: 10.5 MB (66.29%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -2,18 +2,27 @@ class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
         int n=arr.size();
+       
         //sort(arr.begin(),arr.end());
         for(int i=0;i<n-1;i++){
+             bool t=true;
                 for(int j=0;j<n-i-1;j++){
                     int t1=__builtin_popcount(arr[j]);
                     int t2=__builtin_popcount(arr[j+1]);
-                    if(t1>t2)
+                    if(t1>t2){
                     swap(arr[j],arr[j+1]);
+                    t=false;}
                     else if(t1==t2){
-                        if(arr[j]>arr[j+1])
-                            swap(arr[j],arr[j+1]);
+                        if(arr[j]>arr[j+1]){
+                                     swap(arr[j],arr[j+1]);
+                                     t=false;
+                        }
+                           
                     }
+                 
                 }
+                   if(t)
+                    break;
         }
         return arr;
     }
