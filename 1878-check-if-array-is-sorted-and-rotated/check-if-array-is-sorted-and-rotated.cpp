f1583d826d7d70dd03cc456c52978eb67b23101e
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-10-31 21:15:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (21.84%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -5,6 +5,8 @@ public:
         for(int i=1;i<n;i++){
             if(nums[i-1]>nums[i])
             c++;
+            if(c>1)
+            return false;
         }
         if(nums[n-1]>nums[0])
         c++;
