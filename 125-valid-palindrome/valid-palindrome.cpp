c4341e10d9c606bfe128fc8f5eb8c2fbd9cda1c3
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-10-31 22:36:54 +0530

    Time: 9 ms (18.26%) | Memory: 7.9 MB (31.60%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        if(s.length()<=1)
-        return true;
-        int i=0,j=s.length()-1;
-        while(i<=j){
+        int i=0,j=s.length()-1,n=s.length();
+
+        while((i<=j)&&(i<n)&&(j>=0)){
+            // if(!((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z')))
+            // i++;
+            // if(!((s[j]>='a'&&s[j]<='z')||(s[i]>='A'&&s[i]<='Z')))
+            // i++;
             if(!isalnum(s[i]))
             {
                 i++;
                 continue;
             }
-             if(!isalnum(s[j]))
+            if(!isalnum(s[j]))
             {
                 j--;
                 continue;
             }
             if(tolower(s[i])!=tolower(s[j]))
             return false;
-            else{
-                i++;
-                j--;
-            }
-            // cout<<s[i]<<s[j]<<" ";
+            i++;
+            --j;
         }
         return true;
     }
