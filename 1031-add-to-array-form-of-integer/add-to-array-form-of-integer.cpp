c21802788c3b21d08a1916d6d608da2d21b3b5be
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-10-31 21:55:51 +0530

    Time: 22 ms (50.95%) | Memory: 27.5 MB (85.30%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
@@ -2,47 +2,6 @@ class Solution {
 public:
         
     vector<int> addToArrayForm(vector<int>& num, int k) {
-        // int n=num.size(),c=0;
-        // for(int i=n-1;i>=0;i--){
-        //         if(k==0)
-        //         break;
-        //         int t=k%10;
-        //         k/=10;
-        //         t=t+c+num[i];
-        //         c=0;
-        //         if(t>9){
-        //             c=1;
-        //             num[i]=t%10;
-        //         }
-        //         else
-        //         num[i]=t;
-
-        // }
-        // if(c==1){
-        //          num.push_back(0);
-        //          for(int i=n;i>0;i--)
-        //          num[i+1]=num[i];
-        //          num[0]=1;
-        // }
-       
-        // return num;
-        
-
-            //         long long int c=0;
-            // int n=num.size();
-            // for(int i=0;i<n;i++){
-            //         c=(c*10)+num[i];
-            // }
-            // c+=k;
-            // cout<<c;
-            // num.clear();
-            // while(c){
-            //     num.push_back(c%10);
-            //     c=c/10;
-            // }
-            // reverse(num.begin(),num.end());
-            // return num;
-
         int n=num.size(),i=0,c=0;
         reverse(num.begin(),num.end());
        while((k)||(i<n)){
