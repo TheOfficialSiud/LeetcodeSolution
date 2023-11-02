bed6963ad98481b3dad6c9123f20033539e8e70c
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-11-02 20:36:43 +0530

    Time: 16 ms (14.74%) | Memory: 10.1 MB (5.10%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -2,22 +2,6 @@ class Solution {
 public:
 
     bool isPalindrome(int x) {
-        // if(x<0)
-        // return 0;
-        // if(x>=0&&x<=9)
-        // return 1;
-        // int t=pow(10,(int)log10(x));
-        // cout<<t<<endl;
-        // int i=x/t;
-        // x=x%t;
-        // if(x==0)
-        // return false;
-        // int j=x%10;
-        // x=x/10;
-        // cout<<i<<" "<<j<<" "<<x<<endl;
-        // if(i!=j)
-        // return false;
-        // return isPalindrome(x);
         if(x<0)
         return 0;
         if(x>=0&&x<=9)
@@ -30,11 +14,11 @@ public:
         int i=0,j=v.size()-1;
         while(i<=j){
             if(v[i]!=v[j])
-            return false;
+            return 0;
             i++;
             --j;
         }
-        return true;
+        return 1;
 
     }
 };
