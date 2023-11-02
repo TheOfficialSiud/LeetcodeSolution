b8ddf3b23ac933944332f9abb847d959fa4ee409
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-11-02 09:47:45 +0530

    Time: 16 ms (72.98%) | Memory: 19.8 MB (11.86%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& arr) {
-        int i=0,j=0,n=arr.size();
-        while(j<n){
-            if(arr[j]!=0){
-                swap(arr[i],arr[j]);
-                    i++;
+        int *i=new int;
+        int *j=new int;
+        int *n=new int;
+        *i=*j=0;
+        *n=arr.size();
+        while(*j<*n){
+            if(arr[*j]!=0){
+                swap(arr[*i],arr[*j]);
+                    *i+=1;
                 
             } 
-            j++;
+            *j+=1;
         }
     }
 };
