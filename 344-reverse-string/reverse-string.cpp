1e17e16d7aa3848a7ad2c2edd3be3c91cde5f3ec
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-11-02 18:57:50 +0530

    Time: 15 ms (80.28%) | Memory: 23.5 MB (74.14%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reversing(vector<char>& s,int i,int j){
+            if(i>=j)
+            return;
+            swap(s[i],s[j]);
+            return reversing(s,++i,--j);
+    }
+    void reverseString(vector<char>& s) {
+      reversing(s,0,s.size()-1);
+    }
+};
