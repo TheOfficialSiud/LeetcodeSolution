80038d2e0599179d1a1cbad0da332689bafe09dd
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-11-02 20:40:05 +0530

    Time: 7 ms (72.42%) | Memory: 6.3 MB (60.07%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -6,19 +6,14 @@ public:
         return 0;
         if(x>=0&&x<=9)
         return 1;
-        vector<int> v;
+        int n=x;
+        long long v=0;
         while(x){
-            v.push_back(x%10);
+            v=v*10+(x%10);
             x=x/10;
         }
-        int i=0,j=v.size()-1;
-        while(i<=j){
-            if(v[i]!=v[j])
-            return 0;
-            i++;
-            --j;
-        }
-        return 1;
+        cout<<v;
+        return v==n;
 
     }
 };
