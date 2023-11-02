7a75942e2cfd64cd3d9b7446ac64c82a13e0c9b3
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-11-02 12:05:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (8.00%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -1,57 +1,41 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        // int ans=1;
-        // for(int i=0;i<chars.size();i++){
-        //     if(chars[i]==chars[i+1]){
-        //         int j=i,c=0;
-        //         while(j<chars.size()){
-        //             if(chars[j]==chars[j+1])
-        //             c++;
-        //             else
-        //             break;
-        //             j++;
-        //         }
-        //         i=j;
-        //         ans=ans+log10(c)+1;
-        //     }
-        //     else
-        //     ans++;
-        // }
-        // return ans;
-
-       
+        int i=0,j=1,c=1;
+       // chars.push_back('1');
         int n=chars.size();
-         if(n==1)
-        return 1;
-        vector<char> temp;
-        for(int i=0;i<n;i++){
-            temp.push_back(chars[i]);
-            if(i==n-1)
-            continue;
-            if(chars[i]==chars[i+1]){
-                    int c=1,j=i;
-                    while(j<n-1){
-                        if(chars[j]!=chars[j+1])
-                        break;
-                        else
-                        c++;
-                        j++;
-                    }
-                    i=j;
-                    j=log10(c);
-                    int div=pow(10,j);
-                    cout<<div<<" "<<c<<endl;
-                    while(div){
-                        cout<<char((c/div)+48)<<endl;
-                        temp.push_back(char((c/div)+48));
-                       c=c%div;
-                       div=div/10;
-                    }
+        while(j<n){
+            if(chars[j]==chars[j-1]){
+                    c++;
             }
-        }
+            else{
+                
+                chars[i]=chars[j-1];
+                i++;
+                if(c>1){
+                string s=to_string(c);
+                for(auto it:s){
+                    chars[i]=it;
+                    i++;
+                }
+                }
+                c=1;
 
-        chars=temp;
-        return temp.size();
+            }
+            j++;
+        }
+             chars[i]=chars[j-1];
+                i++;
+                if(c>1){
+                string s=to_string(c);
+                for(auto it:s){
+                    chars[i]=it;
+                    i++;
+                }
+                }
+        
+
+        return i;
+
     }
 };
