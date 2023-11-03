56742e2eefb483b1c2e0ebe7f8480e1e316e1876
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-11-03 10:08:33 +0530

    Time: 310 ms (48.91%) | Memory: 154.6 MB (31.44%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,63 +1,40 @@
 class Solution {
 public:
-    // void selctionsort(vector<int> &arr,int i,int n){
-    //     if(i==n-1)
-    //     return;
-    //     for(int j=i;j<n;j++){
-    //         if(arr[i]>arr[j])
-    //         swap(arr[i],arr[j]);
-    //     }
-    //     return selctionsort(arr,++i,n);
-    // }
-    // void bubblesort(vector<int> &arr,int i,int n){
-    //     if(i==n-1)
-    //     return;
-    //     for(int j=0;j<n-i-1;j++)
-    //     {
-    //         if(arr[j]>arr[j+1])
-    //         swap(arr[j],arr[j+1]);
-    //     }
-    //     return bubblesort(arr,++i,n);
-    // }
 
-    void mergearray(vector<int>&arr, int s, int e){
-
-        //now going to merge the sorted array
+    void merge(vector<int> &arr,int s,int e){
         int mid=(s+e)/2;
-        int len1=mid-s+1;//length of 1st array;
-        int len2=e-mid; //length of 2nd array;
+        int len1=mid-s+1;
+        int len2=e-mid;
+        int mainindex=s;
 
-        int mainarrayindex=s;
         int *arr1=new int[len1];
         int *arr2=new int[len2];
 
         for(int i=0;i<len1;i++){
-                arr1[i]=arr[s++];
-        } 
-        s=mid+1;
+            arr1[i]=arr[s++];
+        }
+        
         for(int i=0;i<len2;i++){
-                arr2[i]=arr[s++];
+            arr2[i]=arr[s++];
         }
 
         int index1=0,index2=0;
 
         while(index1<len1||index2<len2){
-
             if(index1>=len1){
-                arr[mainarrayindex++]=arr2[index2++];
+                arr[mainindex++]=arr2[index2++];
             }
             else if(index2>=len2){
-                arr[mainarrayindex++]=arr1[index1++];
+                arr[mainindex++]=arr1[index1++];
             }
-            else {
-                    if(arr1[index1]<arr2[index2]){
-                        arr[mainarrayindex++]=arr1[index1++];
-                    }
-                    else{
-                        arr[mainarrayindex++]=arr2[index2++];
-                    }
+            else{
+                if(arr1[index1]<arr2[index2]){
+                    arr[mainindex++]=arr1[index1++];
+                }
+                else{
+                    arr[mainindex++]=arr2[index2++];
+                }
             }
-
         }
     }
 
@@ -65,22 +42,15 @@ public:
         if(s>=e)
         return;
         int mid=(s+e)/2;
-        //left vala part
+
         mergesort(arr,s,mid);
-        //right vala part
         mergesort(arr,mid+1,e);
 
-        //Now start merging the arrays after they are divided into single - single part
+        merge(arr,s,e);
 
-        mergearray(arr,s,e);
-    
     }
-
-
-    vector<int> sortArray(vector<int>& arr) {
-      //bubblesort(arr,0,arr.size());
-      //mergesort(arr,0,arr.size()-1);
-      sort(arr.begin(),arr.end());
-      return arr;
+    vector<int> sortArray(vector<int>& nums) {
+      mergesort(nums,0,nums.size()-1);
+      return nums;  
     }
 };
