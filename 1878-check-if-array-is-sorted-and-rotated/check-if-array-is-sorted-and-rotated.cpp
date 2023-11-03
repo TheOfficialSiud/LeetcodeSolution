c59fef489a5b5cfe006eb6c0b89963425bd772cf
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-11-03 16:05:22 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (9.09%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
-    bool check(vector<int>& arr) {
-     int c=0,n=arr.size();
-     for(int i=1;i<n;i++){
-         if(arr[i]<arr[i-1])
-         c++;
-     }
-     if(arr[0]<arr[n-1])
-     c++;
-        cout<<c;
-     return ((c==1)||(c==0));
+    void county(vector<int>& nums,int i,int &cnt){
+        if(i==nums.size()-1)
+            {
+                if(nums[0]<nums[i])
+                cnt++;
+                return ;
+            }
+        if(nums[i]>nums[i+1])
+        cnt++;
+        return county(nums,i+1,cnt);
+
+    }
+    bool check(vector<int>& nums) {
+        int cnt=0;
+        county(nums,0,cnt);
+        cout<<cnt;
+        return (cnt==0||cnt==1);
     }
 };
