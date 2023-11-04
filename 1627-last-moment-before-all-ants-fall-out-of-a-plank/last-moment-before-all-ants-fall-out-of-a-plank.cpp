770a24f63a3ad6e8ba43c28f27a6415ad1615781
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-11-04 12:50:35 +0530

    Time: 17 ms (68.53%) | Memory: 23.2 MB (14.66%) - LeetSync

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.cpp
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -3,14 +3,21 @@ public:
     int getLastMoment(int n, vector<int>& left, vector<int>& right) {
      if(right.size()==n||left.size()==n)
      return  n;
-     int maxi=INT_MIN,mini=INT_MAX;
-     for(int i=0;i<left.size();i++){
-            maxi=max(maxi,left[i]);
-     }
-     for(int i=0;i<right.size();i++){
-            mini=min(mini,right[i]);
-     }
 
+    int n1=left.size(),n2=right.size();
+    int i=0,j=0;
+    int maxi=INT_MIN,mini=INT_MAX;
+
+    while(i<n1||j<n2){
+        if(i!=n1){
+             maxi=max(maxi,left[i]);
+             i++;
+        }
+        if(j!=n2){
+             mini=min(mini,right[j]);
+             j++;
+        }
+    }
         return max(maxi,n-mini);
 
 
