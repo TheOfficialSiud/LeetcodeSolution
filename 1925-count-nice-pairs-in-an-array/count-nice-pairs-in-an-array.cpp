6bfed33a90e15c0c187d5ef99e9da290cffcff82
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-11-21 14:21:33 +0530

    Time: 99 ms (54.64%) | Memory: 57.3 MB (35.81%) - LeetSync

--- 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.cpp
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
+   int rev(int n)
+    {
+        int re=0;
+        while(n>0)
+        {
+            re= re*10 + n%10;
+            n/=10;
+        }
+        return re;
+    }  
     int countNicePairs(vector<int>& nums) {
-        // vector<int> temp;
         map<int,int> mp;
         int n=nums.size();
         for(int i=0;i<n;i++){
-            string s=to_string(nums[i]);
-            reverse(s.begin(),s.end());
-            mp[nums[i]-stoi(s)]++;
+            mp[nums[i]-rev(nums[i])]++;
         }
         int ans=0;
         int mod=1e9+7;
         for(auto it:mp)
         {
-        //    int n=it.second;
-        //     n=(long(n%mod)*(long(n-1)/2)%mod)%mod;
         long n=long(it.second);
         n=(n*(n-1)/2);
             cout<<n<<" ";
                 ans=(ans%mod+n%mod)%mod;
         }
+        
         return ans;
     }
 };
