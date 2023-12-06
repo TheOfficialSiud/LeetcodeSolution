b6087447afc444944b639e30d16064d4d3bd2db1
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-12-06 09:21:10 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (35.53%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int sum=0;
+        int t=n/7;
+        n=n%7;
+        sum=t*28+(n*(n+1)/2);
+        cout<<sum;
+        if(n!=0){
+            sum+=t*n;
+        }
+        if(t>0)
+        {
+          --t;
+          sum+=7*(t+1)*t/2;
+        }
+        return sum;
+    }
+};
