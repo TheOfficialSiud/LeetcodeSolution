7f175d4e14aa60eb1f97e4c4d7dbb8bd4cf452c9
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-12-14 09:04:05 +0530

    Time: 202 ms (44.89%) | Memory: 118.4 MB (56.91%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -6,19 +6,30 @@ public:
 
         vector<int> rowOnes(m, 0);
         vector<int> colOnes(n, 0);
+        vector<int> rowzero(m, 0);
+        vector<int> colzero(n, 0);
 
         // Count ones in each row and column
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 rowOnes[i] += grid[i][j];
                 colOnes[j] += grid[i][j];
+                if(grid[i][j]==0){
+                        rowzero[i]++;
+                        colzero[j]++;
+                }
+                
             }
         }
+        // for(int i=0;i<n;i++){
+        //     cout<<rowOnes[i]<<" "<<colOnes[i]<<" "<<rowzero[i]<<" "<<colzero[i]<<endl;
+        // }
 
         // Calculate the difference matrix
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                grid[i][j] = 2 * (rowOnes[i] + colOnes[j]) - m - n;
+                grid[i][j] =rowOnes[i]+colOnes[j]-rowzero[i]-colzero[j];
+                // cout<<rowOnes[i]<<" "<<colOnes[j]<<" "<<rowzero[i]<<" "<<colzero[j]<<endl;
             }
         }
 
