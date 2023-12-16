0af7f1bed523899951a7f30ab3d321b8b59820a6
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-12-16 08:05:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (79.21%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return (s==t);
-        // // while(next_permutation(s.begin(),s.end())){
-        // //     if(s==t){
-        // //         return true;
-        // //     }
-        // //     // if(s==NULL)
-        // //     // return false;
-        // //     // s=
-        // // }
-        // // return false;
-        // map<char,int> mp;
-        // for(int i=0;i<s.length();i++){
-        //     mp[s[i]]++;
-        // }
-        // for(int i=0;i<t.length();i++){
-        //     mp[t[i]]--;
-        // }
-        // for(auto it:mp){
-        //     if(it.second!=0)
-        //     return false;
-        // }
-        // return true;
+        vector<int> a(26,0);
+        vector<int> b(26,0);
+
+        for(int i=0;i<s.length();i++){
+            a[s[i]-'a']++;
+        }
+
+        for(int i=0;i<t.length();i++){
+            b[t[i]-'a']++;
+        }
+
+        return (a==b);
 
     }
 };
