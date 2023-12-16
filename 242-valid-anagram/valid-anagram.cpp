5816552908a1f9f384d54d2d8300760d44ec269c
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-12-16 08:03:21 +0530

    Time: 15 ms (19.45%) | Memory: 7.6 MB (79.21%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // sort(s.begin(),s.end());
-        // // sort(t.begin(),t.end());
-        // if(s==t)
-        // return true;
-        // while(next_permutation(s.begin(),s.end())){
-        //     if(s==t){
-        //         return true;
-        //     }
-        //     // if(s==NULL)
-        //     // return false;
-        //     // s=
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return (s==t);
+        // // while(next_permutation(s.begin(),s.end())){
+        // //     if(s==t){
+        // //         return true;
+        // //     }
+        // //     // if(s==NULL)
+        // //     // return false;
+        // //     // s=
+        // // }
+        // // return false;
+        // map<char,int> mp;
+        // for(int i=0;i<s.length();i++){
+        //     mp[s[i]]++;
         // }
-        // return false;
-        map<char,int> mp;
-        for(int i=0;i<s.length();i++){
-            mp[s[i]]++;
-        }
-        for(int i=0;i<t.length();i++){
-            mp[t[i]]--;
-        }
-        for(auto it:mp){
-            if(it.second!=0)
-            return false;
-        }
-        return true;
+        // for(int i=0;i<t.length();i++){
+        //     mp[t[i]]--;
+        // }
+        // for(auto it:mp){
+        //     if(it.second!=0)
+        //     return false;
+        // }
+        // return true;
+
     }
 };
