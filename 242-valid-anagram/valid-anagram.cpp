376e22ece24c09bf4d967e729c73075aa8364a6c
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-12-16 08:06:52 +0530

    Time: 7 ms (76.44%) | Memory: 7.7 MB (79.21%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,12 +3,11 @@ public:
     bool isAnagram(string s, string t) {
         vector<int> a(26,0);
         vector<int> b(26,0);
+        if(s.length()!=t.length())
+        return 0;
 
         for(int i=0;i<s.length();i++){
             a[s[i]-'a']++;
-        }
-
-        for(int i=0;i<t.length();i++){
             b[t[i]-'a']++;
         }
 
