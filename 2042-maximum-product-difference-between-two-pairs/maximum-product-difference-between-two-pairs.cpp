8d955522a74ecca1f46077fa57d90dfbfa4291d1
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-12-18 07:38:20 +0530

    Time: 17 ms (89.58%) | Memory: 20.8 MB (9.65%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
@@ -1,8 +1,44 @@
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+
         int n=nums.size();
-        return ((nums[n-1]*nums[n-2])-(nums[0]*nums[1]));
+        
+        //finding two greatest integer;
+        int w=0,x=0;
+        int pos=-1;
+        for(int i=0;i<n;i++){
+            if(nums[i]>=w){
+                w=nums[i];
+                pos=i;
+            }
+        }
+         for(int i=0;i<n;i++){
+            if(nums[i]>=x&&i!=pos){
+                x=nums[i];
+            }
+        }
+
+
+
+
+         //finding two smallest integer;
+        int y=INT_MAX,z=INT_MAX;
+        pos=-1;
+        for(int i=0;i<n;i++){
+            if(nums[i]<=y){
+                y=nums[i];
+                pos=i;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]<=z&&i!=pos){
+                z=nums[i];
+            }
+        }
+
+        // cout<<w<<" "<<x<<" "<<y<<" "<<z;
+        return ((w*x)-(y*z));
+        
     }
 };
