fbb02fc6de99e4951b1fc298dfbed37321170c03
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-12-23 08:05:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (35.91%) - LeetSync

---
+++ 1619-path-crossing/path-crossing.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+        // int Nans=0,Eans=0;
+        // for(int i=0;i<path.length();i++){
+        //     if(path[i]=='N'){
+        //         Nans++;
+        //     }
+        //     else if(path[i]=='S'){
+        //         Nans--;
+        //     }
+        //     else if(path[i]=='E'){
+        //         Eans++;
+        //     }
+        //     else if(path[i]=='W'){
+        //         Eans--;
+        //     }
+        //     if(Nans==0&&Eans==0){
+        //         return 1;
+        //     }
+        // }
+        // return 0;
+
+        int x=0,y=0;
+        set<pair<int,int>> st;
+        st.insert({x,y});
+        for(int i=0;i<path.length();i++){
+              if(path[i]=='N'){
+                y++;
+            }
+            else if(path[i]=='S'){
+                y--;
+            }
+            else if(path[i]=='E'){
+                x++;
+            }
+            else if(path[i]=='W'){
+                x--;
+            }
+            int temp=st.size();
+            st.insert({x,y});
+            if(temp==st.size())
+            return 1;
+        }
+
+        return 0;
+        
+    }
+};
