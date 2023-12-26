a64bbbdc76895b1e7da7c0ae0540c0acdda5e371
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2023-12-26 21:43:55 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (58.37%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) 
-    {
-        // string s=to_string(n);
+    int hammingWeight(uint32_t n) {
         int c=0;
-        // cout<<s;
         while(n){
-            n=n&(n-1);
-            c++;
+            if(n%2==1)
+                c++;
+            n=n/2;
         }
-      
         return c;
     }
 };
