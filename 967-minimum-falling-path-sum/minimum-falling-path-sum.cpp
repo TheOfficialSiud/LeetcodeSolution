3fddc1d5a5e9a202496a4e15711e3d4837dae870
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2024-01-19 16:30:24 +0530

    Time: 6 ms (98.98%) | Memory: 12.7 MB (5.13%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -0,0 +1,24 @@
+#pragma GCC optimize("O3", "unroll-loops")
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        const int n = matrix.size();
+        if (n==1) return matrix[0][0];//edge case
+        for (int i=n-2; i>=0; i--){
+            matrix[i][0]+=min(matrix[i+1][0], matrix[i+1][1]);
+            matrix[i][n-1]+=min(matrix[i+1][n-1], matrix[i+1][n-2]);
+            for (int j=1; j < n-1; j++) {
+                matrix[i][j]+=min({matrix[i+1][j-1], matrix[i+1][j], matrix[i+1][j+1]});
+            }
+        }
+        return *min_element(matrix[0].begin(), matrix[0].end());
+    }
+};
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
