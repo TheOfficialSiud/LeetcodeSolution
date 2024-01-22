5300f98643641d0fa74ca0c5de3483e595b602bc
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2024-01-22 16:29:53 +0530

    Time: 25 ms (64.92%) | Memory: 24.8 MB (34.62%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -5,7 +5,7 @@ public:
         int n=nums.size();
 
         sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++){
+        for(int i=1;i<n;i++){
             if(nums[i]==nums[i-1])
             {
                 repeat=nums[i];
