adcbbe4e53c1bd78fad9476a3cde71c486b842c6
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2024-01-22 09:20:13 +0530

    Time: 23 ms (73.89%) | Memory: 24.8 MB (34.62%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int miss,repeat,sum=0;
+        int n=nums.size();
+
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1])
+            {
+                repeat=nums[i];
+            }
+            sum+=nums[i];
+
+        }
+        sum=sum-repeat+nums[0];
+        miss=(n*(n+1))/2-sum;
+
+
+
+        return {repeat,miss};
+
+    }
+};
