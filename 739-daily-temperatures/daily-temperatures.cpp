34ec95c60719b14763cf22ca33667ff9384a7392
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2024-01-31 18:53:42 +0530

    Time: 117 ms (92.62%) | Memory: 100.9 MB (67.32%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& tp) {
+        stack<int> st;
+        int n=tp.size();
+
+        st.push(n-1);
+        vector<int> ans(n,0);
+        for(int i=n-2;i>=0;i--){
+            if(tp[i]<tp[st.top()]){
+                ans[i]=1;
+                st.push(i);
+            }
+            else{
+                while(!st.empty()&&tp[i]>=tp[st.top()]){
+                        st.pop();
+                        
+                }
+                if(st.empty()){
+                    ans[i]=0;
+                   
+                }
+                else{
+                    ans[i]=st.top()-i;
+
+                }
+                 st.push(i);
+            }
+
+
+        }
+
+        return ans;
+
+    }
+};
