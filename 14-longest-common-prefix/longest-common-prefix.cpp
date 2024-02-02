fe087b773ebd0e46680d8d78643b6f6fa6364559
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2024-02-02 16:49:44 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.04%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-       int k=1;
+       int k=0;
        int n=strs.size();
        int j=strs[0].length()-1;
-
+        string ans="";
        while(k<n){
-
+            ans="";
         int i=0;
         while(i<=j&&i<strs[k].length()&&strs[0][i]==strs[k][i]){
+            ans.push_back(strs[0][i]);
             i++;
         }
         j=i-1;
-        cout<<j<<' ';
         k++;
        }
 
-       string ans="";
-       for(int i=0;i<=j;i++){
-           ans.push_back(strs[0][i]);
-       }
+       
+    //    for(int i=0;i<=j;i++){
+    //        ans.push_back(strs[0][i]);
+    //    }
 
        return ans;
     }
