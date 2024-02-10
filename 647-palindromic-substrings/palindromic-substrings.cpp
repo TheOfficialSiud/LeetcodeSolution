0addf470561654a3a15f8624a965865d627f1804
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2024-02-10 11:27:54 +0530

    Time: 426 ms (11.00%) | Memory: 459.4 MB (5.20%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    bool ispalindrome(string s){
-        int i=0,j=s.length()-1;
-        while(i<=j){
-                if(s[i]!=s[j])
-                return false;
-                i++;
-                --j;
-        }
-        return true;
-    }
+     bool ispalindrome(string s){
+         int n=s.length();
+
+         for(int i=0;i<n/2;i++){
+             if(s[i]!=s[n-i-1])
+             return 0;
+         }
+
+         return 1;
+     }
     int countSubstrings(string s) {
-        int ans=0,n=s.length();
-        map<char,int> mp;
-        for(int i=0;i<n;i++)
-        mp[s[i]]++;
-        if(mp.size()==1)
-        return ((n+1)*n)/2;
-        mp.clear();
+
+        int n=s.length();
+        int ans=0;
         for(int i=0;i<n;i++){
             string t="";
             for(int j=i;j<n;j++){
-                    t+=s[j];
-                    if(ispalindrome(t)){
-                        ans++;
-                        cout<<t<<endl;
-                    }
-                    
+                t+=s[j];
+                if(ispalindrome(t)){
+                    ans++;
+
+                }  
+                // else break;     
             }
         }
         return ans;
