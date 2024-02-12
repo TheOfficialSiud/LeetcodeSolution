5ef004dca1129a92476cd7ab1bb8faffac7b2420
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2024-02-12 06:42:42 +0530

    Time: 16 ms (42.65%) | Memory: 22.8 MB (8.76%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -2,29 +2,10 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int curr_el=nums[0],curr_occu=0,el=0,occ=INT_MIN;
+        int n=nums.size();
+        
 
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=curr_el){
-                if(curr_occu>=occ){
-                    occ=curr_occu;
-                    el=curr_el;   
-                }
-                curr_occu=1;
-                curr_el=nums[i];
-               
-            }
-             else{
-                    curr_occu++;
-                }
-        }
-
-        // cout<<curr_el<<" "<<curr_occu;
-        if(curr_occu>occ){
-                    el=curr_el;
-                }
-
-        return el;
+        return nums[n/2];
 
     }
 };
