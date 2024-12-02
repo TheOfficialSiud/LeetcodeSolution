937ba3414a27f9149775262656b347bfafcc5316
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2024-12-02 18:26:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (6.98%) - LeetSync

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -5,12 +5,32 @@ class Solution(object):
         :type searchWord: str
         :rtype: int
         """
+        # c=0
+        # sen_lis=sentence.split(' ')
+        # for item in sen_lis:
+        #     c+=1
+        #     if(item.startswith(searchWord)):
+        #         return c
+        
+        # return -1
+
+
+        sentence+=' '
         c=0
-        sen_lis=sentence.split(' ')
-        for item in sen_lis:
-            c+=1
-            if(item.startswith(searchWord)):
-                return c
+        word=""
+        for item in sentence:
+            
+            if(item==' '):
+                c+=1
+                if(word.startswith(searchWord)):
+                    return c
+                else:
+                    word=""
+            else:
+                word+=item
+        
+        
+            
         
         return -1
 
