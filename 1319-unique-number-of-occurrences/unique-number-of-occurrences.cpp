e5476dce6ff6843afed3f3c246486f6a03ff9d0d
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2024-12-03 11:40:39 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (6.06%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
         unordered_map<int,int> mp;
-        for(int i=0;i<arr.size();i++){
-                mp[arr[i]]++;
-        }
+        for(int i=0;i<arr.size();i++)
+        mp[arr[i]]++;
+
         set<int> st;
+
         for(auto it:mp){
             st.insert(it.second);
         }
-        return (st.size()==mp.size());
+
+        return st.size()==mp.size();
     }
 };
