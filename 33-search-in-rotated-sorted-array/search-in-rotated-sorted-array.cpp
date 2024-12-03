1c32ef208e6fd3d50e9bb48c1f88442e13267670
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2024-12-03 18:34:00 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (6.50%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,35 +1,54 @@
 class Solution {
 public:
-   int binarysearch(vector<int> arr,int s,int e,int target){
-       int mid=(s+e)/2;
-       while(s<=e){
-           if(arr[mid]==target)
-           return mid;
-           else if(arr[mid]<target)
-           s=mid+1;
-           else
-           e=mid-1;
-           mid=(s+e)/2;
-       }
-       return -1;
-   }
-    int search(vector<int>& arr, int target) {
-        int i=0,j=arr.size()-1,mid=(i+j)/2;
-        while(i<j){
+
+    int pivot_calc(vector<int>& arr){
+        int s=0,e=arr.size()-1;
+        int mid=(s+e)/2;
+
+        while(s<e){
+             
             if(arr[mid]>=arr[0]){
-                i=mid+1;
+                s=mid+1;
             }
             else{
-                j=mid;
+                e=mid;
             }
-            mid=(i+j)/2;
+            mid=(s+e)/2;
         }
-        int pivot=i;
+        return mid;
+    }
+    int binary_srch(int s,int e,vector<int>& arr,int target){
+        int mid=0;
+        while(s<=e){
+            mid=(s+e)/2;
+            if(arr[mid]==target){
+                cout<<"s";
+                return mid;
+            }
+            else if(arr[mid]<target){
+                s=mid+1;
+            }
+            else
+                e=mid-1;
+        }
+
+        return -1;
+
+    }
+
+    int search(vector<int>& nums, int target) {
+
+        int pivot=pivot_calc(nums);
         cout<<pivot;
-        int ans=binarysearch(arr,pivot,arr.size()-1,target);
-        //cout<<ans;
-        if(ans==-1)
-        ans=binarysearch(arr,0,pivot-1,target);
-        return ans;;
+        
+        int ind1,ind2;
+
+        ind1=binary_srch(0,pivot-1,nums,target);
+        if(ind1==-1){
+        ind2=binary_srch(pivot,nums.size()-1,nums,target);
+        return ind2;}
+        else
+        return ind1;
+
     }
 };
