bd9ed983dd804433aace1169d7de316c788c7341
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2025-05-24 23:24:40 +0530

    Time: 0 ms (100.00%) | Memory: 77 MB (73.09%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -10,42 +10,108 @@
  */
 class Solution {
 public:
+
+    // ListNode* Reverselist(ListNode* head){
+    //     ListNode *prev=NULL, *curr=head,*nex;
+
+    //     while(curr){
+    //         nex=curr->next;
+    //         curr->next=prev;
+    //         prev=curr;
+    //         curr=nex;
+            
+    //     }
+     
+        
+    //     return prev;
+
+    // }
+
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int carry=0;
+
+        // l1= Reverselist(l1);
+        // l2= Reverselist(l2);
+
+        ListNode *res=new ListNode(),*temp=res;
+       
+        temp=res;
+
         ListNode *t1=l1,*t2=l2;
-        ListNode *ans=new ListNode(-1);
-        ListNode *temp=ans;
-        while(t1||t2){
-            if(t1==NULL){
-                int t=t2->val+carry;
-                t2->val=t%10;
-                carry=t/10;
-                temp->next=t2;
-                t2=t2->next;
-                temp=temp->next;
+        int carry=0;
+
+
+
+        while(t1&&t2){
+            // cout<<1;
+            
+            if((t1->val+t2->val+carry)>9){
+
                 
-            }
-            else if(t2==NULL){
-                int t=t1->val+carry;
-                t1->val=t%10;
-                carry=t/10;
-                temp->next=t1;
-                t1=t1->next;
+                temp->next=new ListNode((t1->val+t2->val+carry)%10);
+                carry=1;
                 temp=temp->next;
+
+
             }
             else{
-                int t=t1->val+t2->val+carry;
-                t1->val=(t)%10;
-                carry=(t)/10;
-                cout<<t1->val<<" "<<carry<<endl;
-                temp->next=t1;
+                temp->next=new ListNode(t1->val+t2->val+carry);
+                carry=0;
                 temp=temp->next;
-                t1=t1->next;
-                t2=t2->next;
             }
+
+            t1=t1->next;
+            t2=t2->next;
         }
-        if(carry==1)
-        temp->next=new ListNode(1);
-        return ans->next;
+
+
+
+
+
+        while(t1){
+            if((t1->val+carry)>9){
+
+                
+                temp->next=new ListNode((t1->val+carry)%10);
+                carry=1;
+                temp=temp->next;
+
+
+            }
+             else{
+                temp->next=new ListNode(t1->val+carry);
+                carry=0;
+                temp=temp->next;
+            }
+            t1=t1->next;
+        }
+
+             while(t2){
+            if((t2->val+carry)>9){
+
+                
+                temp->next=new ListNode((t2->val+carry)%10);
+                carry=1;
+                temp=temp->next;
+
+
+            }
+             else{
+                temp->next=new ListNode(t2->val+carry);
+                carry=0;
+                temp=temp->next;
+            }
+            t2=t2->next;
+        }
+
+
+        if(carry){
+            temp->next=new ListNode(carry);
+                carry=0;
+                temp=temp->next;
+
+        }
+        // cout<<2;
+        return res->next;
+        
     }
 };
