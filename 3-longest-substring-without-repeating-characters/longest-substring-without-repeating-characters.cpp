ffc6d5731cdd968e308dd7e1e77ddd6f981b7160
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2025-05-25 17:17:17 +0530

    Time: 6 ms (71.61%) | Memory: 11.5 MB (83.50%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -5,22 +5,14 @@ public:
 
         int maxi=0;
         int j=0;
-        vector<int> mp(1000,-1);
+        vector<int> mp(256,-1);
 
         for(int i=0;i<n;i++){
-            cout<<i<<" "<<j<<" "<<maxi<<" "<<mp[s[i]];
-            cout<<endl;
             if(mp[s[i]] != -1){
-                cout<<i<<"h"<<endl;
                   j=max(mp[s[i]]+1,j);                
             }
-
             mp[s[i]]=i;
             maxi=max(maxi,i-j+1);
-
-            cout<<i<<" "<<j<<" "<<maxi<<" "<<mp[s[i]];
-            cout<<endl;
-            cout<<endl;
         }
         return maxi;
     }
