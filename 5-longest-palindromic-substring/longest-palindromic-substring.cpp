9abbd78a51b3cc54be567b1efc2bb38c51f58527
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2025-05-25 23:22:22 +0530

    Time: 3 ms (98.13%) | Memory: 9.4 MB (71.41%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,76 +1,54 @@
 class Solution {
 public:
+
     string longestPalindrome(string s) {
-
-        int i=0,j=0,k=0;
-        int n=s.length();
+        int n=s.length(),k=0;
         
-        // int curr=0,ans=0;
-        string ans="";
-
-        //odd case
-
-        while(i<n){
-
-           string curr="";
-           curr.push_back(s[i]);
-            j=i-1;
-            k=i+1;
-            while(j>=0&&k<n){
-                if(s[j]==s[k]){
-                    curr=s[j]+curr+s[k];
-                }
-                else{
-                    break;
-                }
-                ++k;
-                --j;
+        int start=0,ans=0;;
+        while(k<n){
+            int i=k-1,j=k+1;
+            int c1=1;
+            while(i>=0&&j<n){
+                if(s[i]!=s[j])
+                break;
+                c1+=2;
+                --i;
+                ++j;
             }
 
-            if(curr.length()>ans.length()){
-                ans=curr;
+            if(c1>ans)
+           {
+               ans=c1;
+               start=k-c1/2;
+           }
+
+          if(k!=n-1){
+                if(s[k]==s[k+1]){
+                    i=k;
+                    j=k+1;
+                    int c2=0;
+
+
+                 while(i>=0&&j<n){
+                if(s[i]!=s[j])
+                break;
+                c2+=2;
+                --i;
+                ++j;
             }
+
+            if(c2>ans){
+                ans=c2;
+                start=k-(c2/2)+1;
+            }
+                }
+          }
            
-            i++;
+
+
+            k++;
         }
 
-
-        //even case
-
-        i=0;
-        int m=1;
-        while(m<n){
-            if(s[i]==s[m]){
-                string curr="";
-                curr.push_back(s[i]);
-                curr.push_back(s[m]);
-                j=i-1;
-                k=m+1;
-
-                while(j>=0&&k<n){
-                if(s[j]==s[k]){
-                    curr=s[j]+curr+s[k];
-                }
-                else{
-                    break;
-                }
-                ++k;
-                --j;
-            }
-            if(curr.length()>ans.length()){
-                ans=curr;
-            }
-                
-            }
-
-            i++;
-            m++;
-        }
-
-
-
-
-        return ans;
-        
+        return s.substr(start,ans);
     }
 };
