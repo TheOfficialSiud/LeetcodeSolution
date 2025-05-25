d5afa898c438f586f28fab2ac3257658875c4598
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2025-05-25 23:16:47 +0530

    Time: 2277 ms (5.00%) | Memory: 489 MB (5.06%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,66 +1,77 @@
 class Solution {
 public:
-
     string longestPalindrome(string s) {
-        int n=s.length(),k=0;
-        //string ans="";
-        int start=0,ans=0;;
-        while(k<n){
-            int i=k-1,j=k+1;
-            //string str;
-            int c1=1;
-            //str.push_back(s[k]);
-            while(i>=0&&j<n){
-                if(s[i]!=s[j])
-                break;
-                // string t;
-                // t.push_back(s[i]);
-                // t+=str;
-                // t+=s[j];
-                // str=t;
-                c1+=2;
-                --i;
-                ++j;
-            }
-            //cout<<str<<endl;
-            if(c1>ans)
-           {
-               ans=c1;
-               start=k-c1/2;
-           }
 
-          if(k!=n-1){
-                if(s[k]==s[k+1]){
-                    i=k;
-                    j=k+1;
-                    int c2=0;
+        int i=0,j=0,k=0;
+        int n=s.length();
+        
+        // int curr=0,ans=0;
+        string ans="";
 
+        //odd case
 
-                 while(i>=0&&j<n){
-                if(s[i]!=s[j])
-                break;
-                // string t;
-                // t.push_back(s[i]);
-                // t+=str;
-                // t+=s[j];
-                // str=t;
-                c2+=2;
-                --i;
-                ++j;
-            }
+        while(i<n){
 
-            if(c2>ans){
-                ans=c2;
-                start=k-(c2/2)+1;
-            }
+           string curr="";
+           curr.push_back(s[i]);
+            j=i-1;
+            k=i+1;
+            while(j>=0&&k<n){
+                if(s[j]==s[k]){
+                    curr=s[j]+curr+s[k];
                 }
-          }
+                else{
+                    break;
+                }
+                ++k;
+                --j;
+            }
+
+            if(curr.length()>ans.length()){
+                ans=curr;
+            }
            
-
-
-            k++;
+            i++;
         }
 
-        return s.substr(start,ans);
+
+        //even case
+
+        i=0;
+        int m=1;
+        while(m<n){
+            cout<<1;
+            if(s[i]==s[m]){
+                string curr="";
+                curr.push_back(s[i]);
+                curr.push_back(s[m]);
+                j=i-1;
+                k=m+1;
+
+                while(j>=0&&k<n){
+                if(s[j]==s[k]){
+                    curr=s[j]+curr+s[k];
+                }
+                else{
+                    break;
+                }
+                ++k;
+                --j;
+            }
+            if(curr.length()>ans.length()){
+                ans=curr;
+            }
+                
+            }
+
+            i++;
+            m++;
+        }
+
+
+
+
+        return ans;
+        
     }
 };
