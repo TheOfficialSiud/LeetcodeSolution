0b945146152c564d99824675f1be431a9a2d4e96
Sudhanshu Tripathi <98637896+TheOfficialSiud@users.noreply.github.com>
2025-07-22 10:51:40 +0530

    Time: 3 ms (6.06%) | Memory: 9.5 MB (76.46%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.cpp
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -5,7 +5,7 @@ public:
         int n=s.length();
         int r=n;
 
-        while(r!=0){
+        while(r!=0&&i<n){
             int start, end;
             if(r<k){
                 start=i,end=i+r-1;
@@ -20,7 +20,7 @@ public:
                 i=i+2*k;
                 r=r-2*k;
             }
-
+            
             while(start<end){
                     swap(s[start],s[end]);
                     start++;
